d99d494e9cebef8844029342b95cab9603ad0af1
TharunSivamani <tharunsivamani@gmail.com>
2022-09-13 18:55:19 +0530

    Sep-13

---
+++ Arrays/IntersectionArrays.java
@@ -0,0 +1,41 @@
+/*
+349. Intersection of Two Arrays
+
+Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order. 
+
+Example 1:
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2]
+
+Example 2:
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [9,4]
+Explanation: [4,9] is also accepted.
+*/
+
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        
+        Set<Integer> set = new HashSet<>();
+        List<Integer> result = new ArrayList<>();
+        
+        for(int i:nums1){
+            set.add(i);
+        }
+        
+        for(int i:nums2){
+            if(set.contains(i)){
+                result.add(i);
+                set.remove(i);
+            }
+        }
+        
+        int arr[] = new int[result.size()];
+        for(int i=0;i<result.size();i++){
+            arr[i] = result.get(i);
+        }
+        
+        return arr;
+        
+    }
+}
