fb3765ce8ff41860ee5c687b7252b22d476c981a
TharunSivamani <tharunsivamani@gmail.com>
2022-10-18 18:59:28 +0530

    Oct-18

--- Strings/ValidAnagram.java
+++ Strings/ValidAnagram.java
@@ -31,3 +31,29 @@ class Solution {
         return false;
     }
 }
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+        
+        
+        int freq[] = new int[26];
+        
+        for(int i=0;i<s.length();i++){
+            char c1 = s.charAt(i);
+            char c2 = t.charAt(i);
+            
+            freq[c1 - 'a'] += 1;
+            freq[c2 - 'a'] -= 1;            
+        }
+        
+        for(int i : freq){
+            if(i != 0){
+                return false;
+            }
+        }
+        return true;        
+    }
+}
