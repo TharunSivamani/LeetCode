fe8a88a1acfa47456358bd7b5f4007f7497c7894
TharunSivamani <tharunsivamani@gmail.com>
2023-04-12 08:13:58 +0530

    Apr-12

--- Math/SumofNumberandItsReverse.java
+++ Math/SumofNumberandItsReverse.java
@@ -47,3 +47,26 @@ class Solution {
         
     }
 }
+
+class Solution {
+    public boolean sumOfNumberAndReverse(int num) {
+
+        for(int i=0;i<=num;i++){
+            int n = i;
+            int r = 0;
+
+            while(n!=0){
+                int rem = n%10;
+                r = r*10+rem;
+                n/=10;
+            }
+
+            if(r+i == num){
+                return true;
+            }
+        }
+
+        return false;
+        
+    }
+}
